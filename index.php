ea5e7e0d19bc213372b7dd73f7352f90f7e573ac
artisticwrist <georgejoeemmanuel@gmail.com>
2023-09-08 14:17:26 +0100

    final commit

--- index.php
+++ index.php
@@ -4,27 +4,44 @@ require_once __DIR__ . '/config.php';
 
 class API {
     function Select(){
+      
+        $slackName = isset($_GET['slack_name']) ? $_GET['slack_name'] : null;
+        $track = isset($_GET['track']) ? $_GET['track'] : null;
+
         $db = new Connect;
-        $users = array();
-        $data = $db->prepare('SELECT slack_name, DAYNAME(current_day) AS day_name, utc_time, track, github_file_url, github_repo_url, status_code FROM users ORDER BY slack_name');
-        $data->execute();
-        while($OutputData = $data->fetch(PDO::FETCH_ASSOC)){
-            $users[$OutputData['slack_name']] = array(
-                'slack_name' => $OutputData['slack_name'],
-                'current_day' => $OutputData['day_name'], // Use the day_name column from the query
-                'utc_time' => $OutputData['utc_time'],
-                'track' => $OutputData['track'],
-                'github_file_url' => $OutputData['github_file_url'],
-                'github_repo_url' => $OutputData['github_repo_url'],
-                'status_code' => $OutputData['status_code']   
-            );
+
+        // Modify the SQL query to format utc_time as datetime
+        $sql = 'SELECT slack_name, DAYNAME(current_day) AS day_name, CONCAT(current_day, " ", utc_time) AS datetime, track, github_file_url, github_repo_url, status_code FROM users WHERE 1=1';
+
+        if ($slackName !== null) {
+            $sql .= " AND slack_name = :slackName";
         }
-        return json_encode($users);
+
+        if ($track !== null) {
+            $sql .= " AND track = :track";
+        }
+
+        $sql .= ' ORDER BY slack_name';
+
+        $data = $db->prepare($sql);
+
+        if ($slackName !== null) {
+            $data->bindParam(':slackName', $slackName);
+        }
+
+        if ($track !== null) {
+            $data->bindParam(':track', $track);
+        }
+
+        $data->execute();
+
+        while($OutputData = $data->fetch(PDO::FETCH_ASSOC)){
+            echo json_encode($OutputData) . PHP_EOL;
+        }
     }
 }
 
 $API = new API;
 header('Content-type: application/json');
-echo $API->Select();
-
+$API->Select();
 ?>
